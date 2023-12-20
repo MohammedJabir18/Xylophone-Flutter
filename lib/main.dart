58b49d3de6c123af9d36db010603f7f5e72b716c
Mohammed Jabir <jabirahmedz111@gmail.com>
2023-12-20 22:39:01 +0530

    In Assets Folder Music Added and Applied all Buttons

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main(){
   runApp(MyApp());
 }
 
-Expanded textBtn(color){
+Expanded textBtn(color, int play){
   return Expanded(
     child: TextButton(
       style: TextButton.styleFrom(
@@ -15,7 +16,8 @@ Expanded textBtn(color){
         )
       ),
       onPressed: (){
-                    
+        final player = AudioPlayer();
+        player.play(AssetSource('piano_$play.wav'));      
       }, 
       child: Text('')
     ),
@@ -24,7 +26,7 @@ Expanded textBtn(color){
 
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
-
+  
   @override
   State<MyApp> createState() => _MyAppState();
 }
@@ -39,13 +41,13 @@ class _MyAppState extends State<MyApp> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              textBtn(Colors.red[700]),
-              textBtn(Colors.orange[700]),
-              textBtn(Colors.yellow[700]),
-              textBtn(Colors.green[600]),
-              textBtn(Colors.blue[700]),
-              textBtn(Colors.indigo[700]),
-              textBtn(Colors.purple[700]),
+              textBtn(Colors.red[700], 1),
+              textBtn(Colors.orange[700], 2),
+              textBtn(Colors.yellow[700], 3),
+              textBtn(Colors.green[600], 4),
+              textBtn(Colors.blue[700], 5),
+              textBtn(Colors.indigo[700], 6),
+              textBtn(Colors.purple[700], 7),
             ],
           ),),
       ),
